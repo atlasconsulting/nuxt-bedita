f8dacd3216ae3f11fdbc69d5c25efbe742d12f91
batopa <batopa@gmail.com>
2023-12-07 18:06:47 +0100

    fix: use ofetch to recaptcha verify

--- src/runtime/server/utils/recaptcha.ts
+++ src/runtime/server/utils/recaptcha.ts
@@ -1,14 +1,14 @@
 import { useRuntimeConfig } from '#imports';
+import { ofetch } from 'ofetch';
 
 export const recaptchaVerifyToken = async (token: string) => {
     const config = useRuntimeConfig();
 
-    const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    const data = await ofetch('https://www.google.com/recaptcha/api/siteverify', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: `secret=${config.bedita.recaptchaSecret}&response=${token}`,
     });
-    const data = await response.json();
 
     return data.success && data.score > 0.5;
 };
