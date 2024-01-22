8020a4d18c4fb4b2a0f224d9ac98581f0ded96af
batopa <batopa@gmail.com>
2024-01-22 09:31:55 +0100

    chore(playground): reorder env variables

--- src/module.ts
+++ src/module.ts
@@ -58,7 +58,7 @@ export default defineNuxtModule<ModuleOptions>({
   },
 
   setup (options, nuxt) {
-    logger.info('Setting up nuxt-bedita...');
+    logger.start('Setting up nuxt-bedita...');
 
     const runtimeConfig = nuxt.options.runtimeConfig
     runtimeConfig.bedita = defu(runtimeConfig.bedita || {}, {
