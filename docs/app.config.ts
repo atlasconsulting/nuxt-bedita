eebc18e93f1fe529430eec8f40caaba436b3e4de
Alberto Pagliarini <batopa@gmail.com>
2024-02-12 18:10:36 +0100

    docs: introduce documentation (#18)
    * docs: introduce documentation with docus
    
    * docs: add composable page
    
    * docs: add page about server part
    
    * ci: add documentation workflow

---
+++ docs/app.config.ts
@@ -0,0 +1,29 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt BEdita',
+    description: 'Documentation of nuxt-bedita module.',
+    socials: {
+      github: 'atlasconsulting/nuxt-bedita',
+      nuxt: {
+        label: 'Nuxt',
+        icon: 'simple-icons:nuxtdotjs',
+        href: 'https://nuxt.com'
+      }
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
