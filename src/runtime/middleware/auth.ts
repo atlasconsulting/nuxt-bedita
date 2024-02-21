910ace2482a8615046fcdb537e3d0c0db835cfa1
batopa <batopa@gmail.com>
2024-02-21 11:52:07 +0100

    chore: remove unused parameter

--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -4,7 +4,7 @@ import { getSessionConfig } from '../server/utils/session';
 import { UserDataStore } from '../types';
 import { useUserState } from '../states/user';
 
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware(async () => {
   if (process.server) {
     const event = useRequestEvent();
     const session = await useSession(event, getSessionConfig());
