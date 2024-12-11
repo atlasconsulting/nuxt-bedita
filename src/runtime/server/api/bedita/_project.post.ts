f2e967c8370afe0a7cecb2f88f3c77d16991a66d
Alberto Pagliarini <batopa@gmail.com>
2024-12-11 14:51:47 +0100

    feat: support multi project switching client between them

---
+++ src/runtime/server/api/bedita/_project.post.ts
@@ -0,0 +1,20 @@
+import { defineEventHandler, useSession, readBody, setResponseStatus } from 'h3';
+import { getSessionConfig } from '../../utils/session';
+import { createError } from '#imports';
+
+export default defineEventHandler(async (event) => {
+  const session = await useSession(event, getSessionConfig());
+  const body = await readBody(event);
+  if (!body?.project || typeof body.project !== 'string') {
+    throw createError({
+      statusCode: 400,
+      statusMessage: '"project" must be a not empty string',
+    });
+  }
+
+  await session.update({ ...session.data, _project: body.project });
+
+  setResponseStatus(event, 204);
+
+  return {};
+});
