1c196f65d414d383eb78b9dc0113a185f15953a5
Alberto Pagliarini <batopa@gmail.com>
2024-12-11 15:54:06 +0100

    docs: documentation about switching API projects

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,9 +1,13 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
+
   devtools: { enabled: true },
+
   app: {
     baseURL: '/nuxt-bedita/',
   },
+
   modules: [],
+  compatibilityDate: '2024-12-11',
 });
