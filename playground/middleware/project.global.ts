fb124ae564860963cfc0561d48f508736535c26a
Alberto Pagliarini <batopa@gmail.com>
2025-01-15 18:59:51 +0100

    examples(playground): update method signature

--- playground/middleware/project.global.ts
+++ playground/middleware/project.global.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware(async () => {
   if (import.meta.server) {
     const event: H3Event = useRequestEvent() as H3Event;
     const currentProject = useState('currentProject', () => '');
-    const session = await useSession(event, getSessionConfig());
+    const session = await useSession(event, getSessionConfig(event));
     if (session.data?._project) {
       currentProject.value = session.data._project;
     }
